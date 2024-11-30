74e3f7e97175985e560b0235ec240e062d381c82
ritik <ritikmishra836@gmail.com>
2024-11-30 22:42:28 +0530

    30-nov

--- Frontend/airbnb-clone-frontend/src/app/app.config.ts
+++ Frontend/airbnb-clone-frontend/src/app/app.config.ts
@@ -1,9 +1,14 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideAnimations(),
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), provideClientHydration()
+  ]
 };
