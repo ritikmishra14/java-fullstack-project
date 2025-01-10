ea2fde7f03fc695a73b7c635e0a55ebd74c60483
ritik <ritikmishra836@gmail.com>
2025-01-10 21:24:25 +0530

    deleted airbnb clone project

--- Frontend/airbnb-clone-frontend/src/app/shared/font-awesome-icon.ts
+++
@@ -1,5 +0,0 @@
-import { IconDefinition } from "@fortawesome/angular-fontawesome";
-import { faAirbnb } from "@fortawesome/free-brands-svg-icons";
-export const fontAwesomeIcons: IconDefinition[] = [
-  faAirbnb 
-]
