77ca27850c1d0aa0607f38bfcc3a08b54f84f16e
ritik <ritikmishra836@gmail.com>
2025-01-11 00:15:16 +0530

    develop 11 jan

--- Frontend/exam-portal-frontend/src/app/app.component.ts
+++ Frontend/exam-portal-frontend/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatButtonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
