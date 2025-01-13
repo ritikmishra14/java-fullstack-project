428e97d7d074c71a88633d9b6d363109b2058022
ritik <ritikmishra836@gmail.com>
2025-01-13 19:53:12 +0530

    13-jan code changes

--- Frontend/exam-portal-frontend/src/app/pages/signup/signup.component.ts
+++ Frontend/exam-portal-frontend/src/app/pages/signup/signup.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -7,13 +9,40 @@ import { MatInputModule } from '@angular/material/input';
   selector: 'app-signup',
   standalone: true,
   imports: [
+    CommonModule,
         MatButtonModule,        
         MatFormFieldModule,
-        MatInputModule
+        MatInputModule,
+        FormsModule
   ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+
+  constructor(){}
+
+  public user ={
+    username : '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    phone: '',
+    email: ''
+  }
+  ngOnInit(): void {
+    
+    
+  }
+
+  formSubmit(){
+    if(this.user.username === '' || this.user.username === null){
+      alert('user is required');
+    }
+    console.log(this.user);
+    
+  }
+  
+
 
 }
