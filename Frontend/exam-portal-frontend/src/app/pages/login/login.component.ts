61cd013935dbe6b71a30bbefb4ef425682503da1
ritik <ritikmishra836@gmail.com>
2025-01-22 19:51:35 +0530

    22-jan develop

--- Frontend/exam-portal-frontend/src/app/pages/login/login.component.ts
+++ Frontend/exam-portal-frontend/src/app/pages/login/login.component.ts
@@ -1,9 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule
+
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
---
+++ Frontend/exam-portal-frontend/src/app/pages/welcome/welcome.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-welcome',
+  standalone: true,
+  imports: [MatButtonModule],
+  templateUrl: './welcome.component.html',
+  styleUrl: './welcome.component.css'
+})
+export class WelcomeComponent {
+
+}
